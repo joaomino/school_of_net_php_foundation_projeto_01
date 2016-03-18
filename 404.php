9bef36a31459fb162c194d19b511c0d7cd48fd33
Joao Mino <joao_mino@yahoo.com.br>
2016-03-17 23:02:12 -0300

    criado arquivo pagina nao encontrada

---
+++ 404.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div>
+            <h2>Erro 404</h2>
+            <h3>Você tentou acessar uma página inválida.</h3>
+        </div>
+    </div>
+</div>
+<div class="push"></div>
